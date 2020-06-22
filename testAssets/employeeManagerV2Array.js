0c25c3fa274ede5b226a2668c9c4be7fdfae888d
JPLABRADOR <vanhagarsux@gmail.com>
2020-06-22 01:03:51 -0700

    Checkpoint 3 Project Data Files

---
+++ testAssets/employeeManagerV2Array.js
@@ -0,0 +1,8 @@
+module.exports = [
+    {
+        employeeName: "Spike Spiegel",
+        employeePhone: "8001234567",
+        employeeEmail: "Spike@crazymail.com",
+        employeeTitle: "Bounty Hunter",
+    }
+]
